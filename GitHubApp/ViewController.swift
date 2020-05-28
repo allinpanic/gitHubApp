4538361bc2f3e0204ac8ee5454b33a3045251e58
Rodianov <rodianov@MacBook-Pro-Rodianov.local>
2020-05-28 18:15:44 +0300

    Initial commit

---
+++ GitHubApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubApp
+//
+//  Created by Rodianov on 28.05.2020.
+//  Copyright © 2020 Rodionova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
